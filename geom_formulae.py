db291e00da263c3dc2b9e5c326c2d4f8e9d65ed5
professordesJ <des@aims.edu.gh>
2014-08-29 11:56:07 +0000

    geometry!

--- geom_formulae.py
+++ geom_formulae.py
@@ -21,9 +21,43 @@ def square_area(side):
     """
     return side*side
 
+
+
+def equilateral_triangle_perimeter(side):
+    """
+    'equilateral_triangle_perimeter' calculates an equilateral triangle's perimeter given a side length:
+    >>> equilateral_triangle_perimeter(4)
+    12
+    :param side: the side length
+    :return: the perimeter (same units as side length)
+    """
+    return 3*side
+
+
+def equilateral_triangle_area(side):
+    """
+    'equilateral_triangle_area' calculates an equilateral triangle's area given a side length:
+    >>> equilateral_triangle_perimeter(4)
+    12
+    :param side: the side length
+    :return: the perimeter (same units as side length)
+    """
+    return 3**(1/2)*side/4
+
 if __name__ == "__main__":
-    sampleSide = 5
-    print("area:",
+    sampleSide = 4
+    print("sample side:",
+          sampleSide,
+          "  square area:",
           square_area(sampleSide),
-          "perimeter:",
-          square_perimeter(sampleSide))
+          "  square perimeter:",
+          square_perimeter(sampleSide),
+          "  equilateral triangle perimeter:",
+          equilateral_triangle_perimeter(sampleSide),
+          "  equilateral triangle area:",
+          equilateral_triangle_area(sampleSide)
+    )
+
+
+
+
